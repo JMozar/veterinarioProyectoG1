9cf6e5c652c105a003f53ad97648ba2362dcb5eb
JMozar <jhon.moore@unmsm.edu.pe>
2022-07-20 21:45:29 -0500

    Añadiendo atributos a clase Persona

--- src/Modelo/Persona.java
+++ src/Modelo/Persona.java
@@ -3,5 +3,9 @@ package Modelo;
 
 
 public class Persona {
-    
+    private String nombre;
+    private String apellido;
+    private String DNI;
+    private int telefono;
+    private int codigo;
 }
